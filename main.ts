cf5643967593b80018d306c5eb6c58934630e03a
vsidtiladsha <113388330+vsidtiladsha@users.noreply.github.com>
2022-10-13 14:24:48 -0700

    made boat array

--- main.ts
+++ main.ts
@@ -13,32 +13,12 @@ function moveBoat (boatArray: Sprite[]) {
         moveBoatFlag = 0
     }
 }
+let moveBoatFlag = 0
 let iterator = 0
 let cursor: Sprite = null
-let moveBoatFlag = 0
-moveBoatFlag = 1
-tiles.setCurrentTilemap(tilemap`level1`)
-cursor = sprites.create(img`
-    1 1 1 1 1 . . . . . . 1 1 1 1 1 
-    1 1 1 . . . . . . . . . . 1 1 1 
-    1 1 . . . . . . . . . . . . 1 1 
-    1 . . . . . . . . . . . . . . 1 
-    1 . . . . . . . . . . . . . . 1 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    1 . . . . . . . . . . . . . . 1 
-    1 . . . . . . . . . . . . . . 1 
-    1 1 . . . . . . . . . . . . 1 1 
-    1 1 1 . . . . . . . . . . 1 1 1 
-    1 1 1 1 1 . . . . . . 1 1 1 1 1 
-    `, SpriteKind.Player)
-grid.snap(cursor)
-grid.moveWithButtons(cursor)
-let smallboatArray = [sprites.create(img`
+let boatrotateArray = ["up", "up", "up"]
+let currentBoat = 0
+let boatSpriteArray = [[sprites.create(img`
     . . . . . b b b b b b . . . . . 
     . . . b b 9 9 9 9 9 9 b b . . . 
     . . b b 9 9 9 9 9 9 9 9 b b . . 
@@ -72,9 +52,64 @@ let smallboatArray = [sprites.create(img`
     . . b d 5 d 3 3 3 3 5 5 b b . . 
     . . . b b 5 5 5 5 5 5 b b . . . 
     . . . . . b b b b b b . . . . . 
-    `, SpriteKind.Player)]
+    `, SpriteKind.Player)], [sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player), sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player)]]
+tiles.setCurrentTilemap(tilemap`level1`)
+cursor = sprites.create(img`
+    1 1 1 1 1 . . . . . . 1 1 1 1 1 
+    1 1 1 . . . . . . . . . . 1 1 1 
+    1 1 . . . . . . . . . . . . 1 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 1 . . . . . . . . . . . . 1 1 
+    1 1 1 . . . . . . . . . . 1 1 1 
+    1 1 1 1 1 . . . . . . 1 1 1 1 1 
+    `, SpriteKind.Player)
+grid.snap(cursor)
+grid.moveWithButtons(cursor)
 game.onUpdate(function () {
     if (moveBoatFlag == 1) {
-        moveBoat(smallboatArray)
+        moveBoat(boatSpriteArray[currentBoat])
     }
 })
